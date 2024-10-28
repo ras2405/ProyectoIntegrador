75609834b2a8601a251696a68c39de30de4dc442
Daiana Aysa <da.aysa@gmail.com>
2024-10-28 16:47:11 -0300

    feat: highlight selected text

--- caqdas/src/App.tsx
+++ caqdas/src/App.tsx
@@ -1,21 +1,28 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  // const [selectedText, setSelectedText] = useState('');
+  const [highlightedText, setHighlightedText] = useState('');
 
   const handleMouseUp = () => {
     const selection = window.getSelection();
     const selectedText = selection ? selection.toString() : '';
-    
+
     if (selectedText) {
-      console.log(selectedText);
+      // setSelectedText(selectedText);
+      const fullText = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore reiciendis tenetur rerum laudantium voluptate quo, ratione odio obcaecati adipisci iure minima excepturi perspiciatis sunt distinctio, itaque quia blanditiis at? Impedit.";
+
+      const highlighted = fullText.replace(selectedText, `<span class="highlight">${selectedText}</span>`);
+      setHighlightedText(highlighted);
     }
   };
 
   return (
     <>
-      <div onMouseUp={handleMouseUp}>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore reiciendis tenetur rerum laudantium voluptate quo, ratione odio obcaecati adipisci iure minima excepturi perspiciatis sunt distinctio, itaque quia blanditiis at? Impedit.
-      </div>
+      <div 
+        onMouseUp={handleMouseUp} 
+        dangerouslySetInnerHTML={{ __html: highlightedText || "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore reiciendis tenetur rerum laudantium voluptate quo, ratione odio obcaecati adipisci iure minima excepturi perspiciatis sunt distinctio, itaque quia blanditiis at? Impedit." }} />
     </>
   )
 }
